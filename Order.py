f815f229028e6e7889ad09841390534c0ac49cf3
Arina Lizunova <achesdox@gmail.com>
2023-05-20 18:03:47 +0300

    Изменен класс заказа. изменен тип полей, добавлено поле id.

--- Order.py
+++ Order.py
@@ -1,11 +1,16 @@
 from dataclasses import dataclass
+from typing import Optional
 
 
 @dataclass
 class Order:
-    area: int
-    grocery_retailer_id: int
-    current_product: bool  # Product
-    time_cooking: int
-    price: float
-    
+    __id: int
+    grocery_retailer_id: Optional[int] = None
+    area: Optional[int] = None
+    current_product: Optional[bool] = None  # Product
+    time_cooking: Optional[int] = None
+    price: Optional[float] = None
+
+    @property
+    def id(self) -> int:
+        return self.__id
