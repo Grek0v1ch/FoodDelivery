da65ef591c5f9fdb4aa244c544a3e93fd46544db
Ivan Voronov <ivvoronov2308@gmail.com>
2023-05-25 19:01:03 +0300

    Refactoring

--- GroceryRetailerManager.py
+++ GroceryRetailerManager.py
@@ -9,7 +9,8 @@ class GroceryRetailerManager:
         self.__retailers: list[GroceryRetailer] = []
 
     def __str__(self):
-        return f'Список магазинов: {self.__retailers}'
+        return f'Список магазинов:\n' + '\n'.join(
+            [str(item) for item in self.__retailers])
 
     def add_grocery_retailer(self, retailer: GroceryRetailer):
         self.__retailers.append(retailer)
@@ -18,17 +19,17 @@ class GroceryRetailerManager:
         with open('resources/GroceryRetailers.json', 'r') as file:
             data = json.load(file)
         for retailer in data:
-            self.__retailers.append(
+            self.add_grocery_retailer(
                 GroceryRetailer(
                     retailer['name'],
                     retailer['type'],
-                    self.load_product_list(retailer['menu']),
-                    self.load_product_list(retailer['stop_list'])
+                    self.__load_product_list(retailer['menu']),
+                    self.__load_product_list(retailer['stop_list'])
                 )
             )
 
     @staticmethod
-    def load_product_list(data: list[dict]) -> list[Product]:
+    def __load_product_list(data: list[dict]) -> list[Product]:
         result: list[Product] = []
         for product in data:
             result.append(
