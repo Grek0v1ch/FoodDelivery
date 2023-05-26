703626e75c7b518116c687256d27f79578434b7b
lomtevmatvei5 <matvei.lomtev@yandex.ru>
2023-05-26 19:33:31 +0300

    написал свойство is_any_none в классе Order.py

--- Order.py
+++ Order.py
@@ -14,3 +14,9 @@ class Order:
     @property
     def id(self) -> tuple[str]:
         return self.__id
+
+    @property
+    def is_any_none(self) -> bool:
+        return self.grocery_retailer_id is None or self.area is None or \
+               self.current_product is None or self.time_cooking is None or\
+               self.price is None
