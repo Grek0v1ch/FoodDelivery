6b4bc9ff4dda8f21713c6172ddb096ca14a9ca14
lomtevmatvei5 <matvei.lomtev@yandex.ru>
2023-05-30 14:41:03 +0300

    добавил поле time_cooking в класс Product.py

--- Product.py
+++ Product.py
@@ -8,13 +8,16 @@ class Product:
     price: Optional[float] = None
     weight: Optional[int] = None
     composition: Optional[str] = None
+    time_cooking: Optional[int] = None
 
     def __str__(self):
         return f'Имя: {self.name}, Стоимость: {self.price}, ' \
-               f'Вес: {self.weight}, Состав: {self.composition} '
+               f'Вес: {self.weight}, Состав: {self.composition}, ' \
+               f'Время приготовления: {self.time_cooking}'
 
     @property
     def additional_information(self):
         return f'Стоимость продукта составляет: {self.price}\n' \
                f'Вес продукта составляет: {self.weight}\n' \
-               f'Состав продукта: {self.composition}'
+               f'Состав продукта: {self.composition}\n' \
+               f'Время приготовления продукта: {self.time_cooking}'
