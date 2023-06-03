9c614aa0f5ce8cb4b97116e3ac013a6e216ce291
Ivan Voronov <ivvoronov2308@gmail.com>
2023-06-03 18:58:59 +0300

    Добавлен класс GeneralManager

---
+++ GeneralManager/GeneralManager.py
@@ -0,0 +1,55 @@
+import json
+import random
+
+from DeliveryManager.DeliveryManager import DeliveryManager
+from DeliveryManager.DeliveryMan import DeliveryMan
+from GroceryRetailer.GroceryRetailerManager import GroceryRetailerManager
+from System import System
+from MetaSingleton import MetaSingleton
+
+
+class GeneralManager(metaclass=MetaSingleton):
+    def __init__(self):
+        self.__delivery_manager: DeliveryManager = DeliveryManager()
+        self.__grocery_manager: GroceryRetailerManager = \
+            GroceryRetailerManager()
+        self.__load_retailers('resources/GroceryRetailers.json')
+        self.__load_delivery_mans('DeliveryMans.json')
+
+    def __load_retailers(self, file_path: str) -> None:
+        self.__grocery_manager.load_json(file_path)
+
+    def __load_delivery_mans(self, file_path: str) -> None:
+        delivery_manager = DeliveryManager()
+        with open(file_path, 'r') as f:
+            text = json.load(f)
+        for i in range(len(text)):
+            man = DeliveryMan(text[i]["area"], text[i]["current_order"],
+                              text[i]["transport"],
+                              text[i]["order_time"], text[i]["time_start"])
+            delivery_manager.add_deliveryman(man)
+
+    def start(self):
+        while True:
+            print('Меню:',
+                  '1. Сделать новый заказ',
+                  '2. Уточнить статус заказов', sep='\n')
+            chose = System.validate_integer_in_range(1, 2)
+            System.clear_terminal()
+            if chose == 1:
+                order = self.__grocery_manager.make_order()
+                length = random.randint(0, 3000)
+                if self.__delivery_manager.accept_order(order, length) is None:
+                    print('Свободных доставщиков в вашем районе пока нет, '
+                          'идет поиск...')
+                    while self.__delivery_manager.accept_order(order, length) is None:
+                        print(self.__delivery_manager.get_order_status(order))
+                        time.sleep(5)
+                        delivery_manager.tick()
+                # Debug
+                print(self.__delivery_manager.get_order_status(order))
+            elif chose == 2:
+                print(self.__delivery_manager.get_order_status(order))
+                _ = input()
+                System.clear_terminal()
+            self.__delivery_manager.tick()
--- main.py
+++ main.py
@@ -2,47 +2,13 @@ import json
 import random
 import time
 
-from DeliveryManager.DeliveryMan import DeliveryMan
-from DeliveryManager.DeliveryManager import DeliveryManager
-from GroceryRetailer.GroceryRetailerManager import GroceryRetailerManager
+from GeneralManager.GeneralManager import GeneralManager
 from System import System
 
 
 def main():
-    manager = GroceryRetailerManager()
-    manager.load_json('resources/GroceryRetailers.json')
-    delivery_manager = DeliveryManager()
-    with open('DeliveryMans.json', 'r') as f:
-        text = json.load(f)
-
-    for i in range(len(text)):
-        man = DeliveryMan(text[i]["area"], text[i]["current_order"],
-                          text[i]["transport"],
-                          text[i]["order_time"], text[i]["time_start"])
-        delivery_manager.add_deliveryman(man)
-    while True:
-        print('Меню:',
-              '1. Сделать новый заказ',
-              '2. Уточнить статус заказов', sep='\n')
-        chose = System.validate_integer_in_range(1, 2)
-        System.clear_terminal()
-        if chose == 1:
-            order = manager.make_order()
-            length = random.randint(0, 3000)
-            if delivery_manager.accept_order(order, length) is None:
-                print('Свободных доставщиков в вашем районе пока нет, '
-                      'идет поиск...')
-                while delivery_manager.accept_order(order, length) is None:
-                    print(delivery_manager.get_order_status(order))
-                    time.sleep(5)
-                    delivery_manager.tick()
-            # Debug
-            print(delivery_manager.get_order_status(order))
-        elif chose == 2:
-            print(delivery_manager.get_order_status(order))
-            _ = input()
-            System.clear_terminal()
-        delivery_manager.tick()
+    manager = GeneralManager()
+    manager.start()
 
 
 if __name__ == "__main__":
