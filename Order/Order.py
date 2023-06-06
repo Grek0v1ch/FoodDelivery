25b674e1b44fe53312df07c4f48122696df1758f
lomtevmatvei5 <matvei.lomtev@yandex.ru>
2023-06-06 16:46:35 +0300

    добавил комментарии

--- Order/Order.py
+++ Order/Order.py
@@ -6,16 +6,36 @@ from Order.Product import Product
 
 @dataclass
 class Order:
+    """
+    Класс для представления заказа.
+
+    Атрибуты:
+    grocery_retailer_id : str
+        id магазина или ресторана для заказа
+    area : int
+        район
+    products : list
+        составляющие заказа
+    weight : int
+        вес заказа
+    time_cooking : int
+        время приготовления заказа
+    price : int
+        стоимость заказа
+    """
     __id: tuple[str]
     grocery_retailer_id: Optional[tuple[str]] = None
     area: Optional[int] = None
     products: Optional[list[Product]] = None
     weight: Optional[int] = None
-    ime_cooking: Optional[int] = None
+    time_cooking: Optional[int] = None
     price: Optional[float] = None
 
     @property
     def get_products_name(self) -> str:
+        """
+        Возвращает составляющие заказа через запятую.
+        """
         result = []
         for product in self.products:
             result.append(product.name)
@@ -27,10 +47,26 @@ class Order:
 
     @property
     def is_any_none(self) -> bool:
+        """
+        Проверяет есть ли какое-либо поле, равное None.
+
+        Возвращаемое значение:
+        True, если хотя бы одно поле равно none.
+        False в противном случае.
+        """
         return self.grocery_retailer_id is None or self.area is None or \
-               self.products is None or self.time_cooking is None or \
-               self.price is None
+            self.products is None or self.time_cooking is None or \
+            self.price is None
 
     @property
     def is_valid_order(self) -> bool:
+        """
+        Проверяет, что заказ валидный.
+        Заказ валидный, если в нем нет полей со значением None
+        и если список продуктов в нем не пустой.
+
+        Возвращаемое значение:
+        True, если заказ валидный.
+        False в противном случае.
+        """
         return not self.is_any_none and len(self.products) != 0
--- Order/OrderBuilder.py
+++ Order/OrderBuilder.py
@@ -5,10 +5,21 @@ from Order.Product import Product
 
 
 class OrderBuilder:
+    """
+    Класс для сборки заказа.
+    """
     def __init__(self):
         self.__order: Order = Order(CreatorID.generate_order_id())
 
     def add_grocery_retailer(self, grocery_retailer: GroceryRetailer):
+        """
+        Создает заказ из указанного магазина, устанавливает начальные значения
+        для объекта order.
+
+        Параметры:
+        grocery_retailer : GroceryRetailer
+            магазин или ресторан, из которого будет сделан заказ
+        """
         self.__order.grocery_retailer_id = grocery_retailer.id
         self.__order.area = grocery_retailer.area
         self.__order.price = 0
@@ -17,6 +28,14 @@ class OrderBuilder:
         self.__order.products = []
 
     def add_product(self, product: Product):
+        """
+        Добавляет продукт в заказ, изменяет значения для объекта order,
+        если указан id магазина или ресторана.
+
+        Параметры:
+        product : Product
+            добавляемый продукт
+        """
         if self.__order.grocery_retailer_id is None:
             return
         self.__order.products.append(product)
@@ -26,4 +45,7 @@ class OrderBuilder:
 
     @property
     def order(self) -> Order:
+        """
+        Возвращает заказ.
+        """
         return self.__order
